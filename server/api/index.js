085b7f6a364f071dc4c6326a3192c978f86aa7cb
YaelCervantes <elmer.eyca@gmail.com>
2026-04-13 11:07:38 -0600

    fix: reemplazar app.options('*') con '(.*)' por incompatibilidad path-to-regexp v8

--- server/api/index.js
+++ server/api/index.js
@@ -18,7 +18,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }))
-app.options('*', cors())
+app.options('(.*)', cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(cors({
   credentials: true,
 }))
 
-app.options('*', cors())
+app.options('(.*)', cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
