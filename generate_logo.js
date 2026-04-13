0662f518d435faaa49854b1e732f149283e8cfdd
YaelCervantes <elmer.eyca@gmail.com>
2026-04-13 13:00:04 -0600

    cleanup: eliminar script temporal generate_logo.js

--- generate_logo.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const dir = path.join(__dirname, 'admin/public/images/logo');
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir, { recursive: true });
-}
-const base64Data = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=";
-fs.writeFileSync(path.join(dir, 'logo.png'), Buffer.from(base64Data, 'base64'));
-console.log('Dummy logo.png created.');
